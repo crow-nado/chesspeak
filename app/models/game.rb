9b502776d05d93700fae0f9de369d3e640fe1126
Bryce Gust <bgust9@gmail.com>
2019-11-12 05:45:41 +0000

    Added Game#populate! method

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,8 +1,25 @@
 class Game < ApplicationRecord
   has_many :pieces
-  has_many :users
 
   def populate!
-    
+    populate_white_side
+    populate_black_side
+  end
+
+  def populate_white_side
+    populate_row(1)
+    populate_row(2)
+  end
+
+  def populate_black_side
+    populate_row(8)
+    populate_row(7)
+  end
+
+
+  def populate_row(row)
+    8.times do |column|
+      self.pieces.create(x_position: column+1, y_position: row)
+    end
   end
 end
