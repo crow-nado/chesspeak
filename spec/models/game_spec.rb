8d47ef838c19bdcabd23edf5d2bba49bd3910c33
Bryce Gust <bgust9@gmail.com>
2019-11-12 05:45:41 +0000

    Added Game#populate! method

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Game, type: :model do
   describe "#populate!" do
     it "populates the game with pieces" do
-      game = Game.new
+      game = Game.create()
       game.populate!
       expect(game.pieces.count).to eq 32
     end
