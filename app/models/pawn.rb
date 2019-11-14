42cdac506d9d7139fe2ec1db6f14c4b448213e7a
Bryce Gust <bgust9@gmail.com>
2019-11-14 18:48:36 +0000

    Added method and test to set 'image' of a piece without migration

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,3 +1,12 @@
 class Pawn < Piece
   belongs_to :game
+  after_initialize :set_image
+  attr_accessor :image
+
+  private
+
+  def set_image
+    return self.image = "&#9817" if self.y_position== 2
+    return self.image = "&#9823" if self.y_position == 7
+  end
 end
--- spec/models/pawn_spec.rb
+++ spec/models/pawn_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Pawn, type: :model do
-
+  describe "#set_image" do
+    it "sets the pawns image in unicode according to its row" do
+      game = Game.create()
+      game.populate_white_pawns
+      expect(game.pawns.find_by(y_position: 2).image).to eq "&#9817"
+    end
+  end
 end
