202d2ee679b2f621d688607396040ced9af0bf05
Bryce Gust <bgust9@gmail.com>
2019-11-19 04:56:44 +0000

    Changed a tags to rails link_to helpers to correctly route users on the static_pages#index page

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -296,5 +296,4 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
-  config.authentication_keys = [ :login ]
 end
