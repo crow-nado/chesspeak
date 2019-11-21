40d27c44b3a056ad80b86af311d3d6239064773d
Bryce Gust <bgust9@gmail.com>
2019-11-21 18:21:46 +0000

    Removed shallow routing to get individual pieces url

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,8 +1,12 @@
 class PiecesController < ApplicationController
   skip_before_action :verify_authenticity_token
-  def show
-    @game = Game.find(params[:id])
+  def index
+    @game = Game.find(params[:game_id])
     render json: @game.pieces
+  end
+
+  def show
+
   end
 
   def update
