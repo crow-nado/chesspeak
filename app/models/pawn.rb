9fa7a83d0dd54471823483b79ab139527160a847
Bryce Gust <bgust9@gmail.com>
2019-12-16 02:04:04 +0000

    Implemented Pawn#add_enemy_diagonal. Final Refactor

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,7 +1,7 @@
 class Pawn < Piece
   belongs_to :game
-  #alias x x_position
-  #alias y y_position
+  alias_attribute :x, :x_position
+  alias_attribute :y, :y_position
 
   def image
     return self.image = "&#9817" if self.is_white_piece?
@@ -12,28 +12,16 @@ class Pawn < Piece
     @valid_moves = []
     add_enemy_diagonal
     if self.is_white_piece?
-      @valid_moves.push({x: self.x_position, y: self.y_position+1})
-      @valid_moves.push({x: self.x_position, y: self.y_position+2}) if self.first_move?
+      @valid_moves.push({x: x, y: y+1})
+      @valid_moves.push({x: x, y: y+2}) if self.first_move?
     else
-      @valid_moves.push({x: self.x_position, y: self.y_position-1})
-      @valid_moves.push({x: self.x_position, y: self.y_position-2}) if self.first_move?
+      @valid_moves.push({x: x, y: y-1})
+      @valid_moves.push({x: x, y: y-2}) if self.first_move?
     end
-    return @valid_moves
-  end
-
-  def has_enemy_diagonal
-    if self.is_white_piece?
-      if self.game.check_square(self.x_position+1, self.y_position+1).nil?
-        return false
-      end
-      square = !self.game.check_square(self.x_position+1, self.y_position+1).is_white_piece?
-    else
-      return self.game.check_square(self.x_position-1, self.y_position-1).is_white_piece?
-    end
+    @valid_moves
   end
 
   def add_enemy_diagonal
-    x,y = self.x_position, self.y_position
     if self.is_white_piece?
       check_and_add_square(x-1, y+1)
       check_and_add_square(x+1, y+1)
