8216b7ce3b1c9746984111f0c52ecdc7eccbe1e0
Bryce Gust <bgust9@gmail.com>
2019-12-24 00:36:55 +0000

    Refactored Knight#valid_moves

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -2,7 +2,22 @@ class Knight < Piece
   belongs_to :game
 
   def valid_moves
-    @valid_moves = [{x: 4, y:7}, {x:6, y:7}, {x:4,y:3}, {x:6,y:3},{x:7,y:4},{x:7,y:6},{x:3,y:4},{x:3,y:6}]
+    @valid_moves = []
+    add_L_pattern(-1,2)
+    add_L_pattern(1,2)
+    @valid_moves
+  end
+
+  def rotate_90(delta_x,delta_y)
+    new_delta_x, new_delta_y = delta_y, (-delta_x)
+  end
+
+  def add_L_pattern(delta_x, delta_y)
+    4.times do
+      square = self.game.check_square(x + delta_x, y + delta_y)
+      @valid_moves.push({x: x + delta_x, y: y + delta_y}) unless is_friendly(square)
+      delta_x, delta_y = rotate_90(delta_x, delta_y)
+    end
   end
 
 end
--- spec/models/knight_spec.rb
+++ spec/models/knight_spec.rb
@@ -26,5 +26,23 @@ RSpec.describe Knight, type: :model do
 	    expect(knight.valid_move?(3,4)).to be true
 	    expect(knight.valid_move?(3,6)).to be true 
     end
+    it "cannot move onto its own piece" do
+	    game = Game.create()
+	    pawn = FactoryBot.create :sample_white_pawn,
+		    x_position: 3, y_position: 4, game_id: game.id
+	    knight = FactoryBot.create :sample_white_knight,
+		    x_position: 2, y_position: 2, game_id: game.id
+	    expect(knight.valid_move?(3,4)).to be false
+	    expect(knight.valid_move?(1,4)).to be true
+    end
+    it "can move onto enemy pieces" do
+	    game = Game.create() 
+      pawn = FactoryBot.create :sample_white_pawn,
+		    x_position: 3, y_position: 4, game_id: game.id
+	    knight = FactoryBot.create :sample_black_knight,
+		    x_position: 2, y_position: 2, game_id: game.id
+	    expect(knight.valid_move?(3,4)).to be true
+	    expect(knight.valid_move?(1,4)).to be true
+    end
   end
 end
